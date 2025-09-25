4756af1462cca9580d53880eec93644619947041
NySuS-Mu <serenity.mir4@gmail.com>
2025-09-24 23:11:17 -0300

    Add files via upload

---
+++ FlyingDragons.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Map.h"
+
+class cDragonMaps
+{
+public:
+    //Dragones métodos
+    void DeleteFlyingDragons ();
+    void AddFlyingDragons(int mapa, int invasionTime, int index);
+    void FlyingDragonsBossDieProc(int mapa);
+    void FlyingDragonsCheck(int mapa, int index);
+private:
+    //Dragones variables de uso
+    struct Maps
+    {
+        int Ok;
+        time_t EndTime;
+        int index;
+    };
+
+    Maps mapasDragones[MAX_MAP];
+};
+
+extern cDragonMaps gDragonMaps;
